114124d39a7bd60d2331b5468bc0d716b2a69811
Bart van Zon <bartvanzon@gmail.com>
2013-10-20 18:38:51 +0200

    initial commit

---
+++ NSDate+RelativeTime.m
@@ -0,0 +1,13 @@
+//
+//  NSDate+RelativeTime.m
+//  NSDate-RelativeTime
+//
+//  Created by Bart van Zon on 10/20/13.
+//  Copyright (c) 2013 Bart van Zon. All rights reserved.
+//
+
+#import "NSDate+RelativeTime.h"
+
+@implementation NSDate (RelativeTime)
+
+@end
