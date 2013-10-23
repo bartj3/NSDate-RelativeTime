99296903bb1752714470a7f897fccdf1a230a92d
Bart van Zon <bartvanzon@gmail.com>
2013-10-23 22:43:50 +0200

    minor refactor and started on simple basic test

--- NSDate+RelativeTime.m
+++ NSDate+RelativeTime.m
@@ -10,9 +10,19 @@
 
 @implementation NSDate (RelativeTime)
 
+const uint MINUTE = 60;
+const uint HOUR = 60*MINUTE;
+const uint DAY = HOUR*24;
+const uint WEEK = DAY*7;
+const uint MONTH = DAY*30;
+const uint YEAR = DAY*365;
+
 -(NSString *)relativeTime
 {
-    return @"some time ago";
+    NSDate *now = [NSDate date];
+    double deltaSeconds = fabs([self timeIntervalSinceDate:now]);
+    
+    return @"now";
 }
 
 @end
--- Tests/NSDate-RelativeTimeTests/NSDate+RelativeTimeTests.m
+++ Tests/NSDate-RelativeTimeTests/NSDate+RelativeTimeTests.m
@@ -3,17 +3,32 @@
 
 SPEC_BEGIN(RelativeTimeSpec)
 
+const uint MINUTE = 60;
+const uint HOUR = 60*MINUTE;
+const uint DAY = HOUR*24;
+const uint WEEK = DAY*7;
+const uint MONTH = DAY*30;
+const uint YEAR = DAY*365;
+
 describe(@"The NSDate class", ^{
-    __block NSDate *date;
-    
-    beforeEach(^{
-        date = [[NSDate alloc] init];
-    });
     
     context(@"with a RelativeTime category", ^{
         
         it(@"should respond to the relativeTime method", ^{
+            NSDate *date = [[NSDate alloc] init];
             [[date should] respondToSelector:@selector(relativeTime)];
+        });
+        
+        it(@"should parse 2 seconds from now as now", ^{
+            NSDate *date = [[NSDate date] dateByAddingTimeInterval:2];
+            
+            [[[date relativeTime] should] equal:@"now"];
+        });
+        
+        it(@"should parse 2 seconds ago as now", ^{
+            NSDate *date = [[NSDate date] dateByAddingTimeInterval:-2];
+            
+            [[[date relativeTime] should] equal:@"now"];
         });
     });
 });
