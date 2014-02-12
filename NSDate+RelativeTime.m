c399fe3122e569f0bfe3d1fc973e0ec4fc892231
Erik Johansson <erik@peacock.se>
2014-02-12 16:24:00 +0100

    added time masks

--- NSDate+RelativeTime.m
+++ NSDate+RelativeTime.m
@@ -20,37 +20,42 @@ const int YEAR = DAY*365;
 
 -(NSString *)relativeTime
 {
+    return [self relativeTimeWithMask:RTTimeMaskSeconds|RTTimeMaskMinutes|RTTimeMaskHours|RTTimeMaskDays|RTTimeMaskWeeks|RTTimeMaskMonths];
+}
+
+-(NSString *)relativeTimeWithMask:(RTTimeMask)mask
+{
     NSDate *currentDate = [NSDate date];
     int deltaSeconds = fabs(lroundf([self timeIntervalSinceDate:currentDate]));
     BOOL dateInFuture = ([self timeIntervalSinceDate:currentDate] > 0);
     
-    if(deltaSeconds < 2*SECOND) {
+    if(mask & RTTimeMaskSeconds && deltaSeconds < 2*SECOND) {
         return [self NSDateRelativeTimeLocalizedStrings: @"Now"];
-    } else if(deltaSeconds < MINUTE) {
+    } else if(mask & RTTimeMaskSeconds && deltaSeconds < MINUTE) {
         return [self formattedStringForCurrentDate:currentDate count:deltaSeconds past:@"%d seconds ago" future:@"%d seconds from now"];
-    } else if(deltaSeconds < 1.5*MINUTE) {
+    } else if(mask & RTTimeMaskMinutes && deltaSeconds < 1.5*MINUTE) {
         return !dateInFuture ? [self NSDateRelativeTimeLocalizedStrings: @"A minute ago"] : [self NSDateRelativeTimeLocalizedStrings: @"A minute from now"];
-    } else if(deltaSeconds < HOUR) {
+    } else if(mask & RTTimeMaskMinutes && deltaSeconds < HOUR) {
         int minutes = (int)lroundf((float)deltaSeconds/(float)MINUTE);
         return [self formattedStringForCurrentDate:currentDate count:minutes past:@"%d minutes ago" future:@"%d minutes from now"];
-    } else if(deltaSeconds < 1.5*HOUR) {
+    } else if(mask & RTTimeMaskHours && deltaSeconds < 1.5*HOUR) {
         return !dateInFuture ? [self NSDateRelativeTimeLocalizedStrings: @"An hour ago"] : [self NSDateRelativeTimeLocalizedStrings: @"An hour from now"];
-    } else if(deltaSeconds < DAY) {
+    } else if(mask & RTTimeMaskHours && deltaSeconds < DAY) {
         int hours = (int)lroundf((float)deltaSeconds/(float)HOUR);
         return [self formattedStringForCurrentDate:currentDate count:hours past:@"%d hours ago" future:@"%d hours from now"];
-    } else if(deltaSeconds < 1.5*DAY) {
+    } else if(mask & RTTimeMaskDays && deltaSeconds < 1.5*DAY) {
         return !dateInFuture ? [self NSDateRelativeTimeLocalizedStrings: @"A day ago"] : [self NSDateRelativeTimeLocalizedStrings: @"A day from now"];
-    } else if(deltaSeconds < WEEK) {
+    } else if(mask & RTTimeMaskDays && deltaSeconds < WEEK) {
         int days = (int)lroundf((float)deltaSeconds/(float)DAY);
         return [self formattedStringForCurrentDate:currentDate count:days past:@"%d days ago" future:@"%d days from now"];
-    } else if(deltaSeconds < 1.5*WEEK) {
+    } else if(mask & RTTimeMaskWeeks && deltaSeconds < 1.5*WEEK) {
         return !dateInFuture ? [self NSDateRelativeTimeLocalizedStrings: @"A week ago"] : [self NSDateRelativeTimeLocalizedStrings: @"A week from now"];
-    } else if(deltaSeconds < MONTH) {
+    } else if(mask & RTTimeMaskWeeks && deltaSeconds < MONTH) {
         int weeks = (int)lroundf((float)deltaSeconds/(float)WEEK);
         return [self formattedStringForCurrentDate:currentDate count:weeks past:@"%d weeks ago" future:@"%d weeks from now"];
-    } else if(deltaSeconds < 1.5*MONTH) {
+    } else if(mask & RTTimeMaskMonths && deltaSeconds < 1.5*MONTH) {
         return !dateInFuture ? [self NSDateRelativeTimeLocalizedStrings: @"A month ago"] : [self NSDateRelativeTimeLocalizedStrings: @"A month from now"];
-    } else if(deltaSeconds < YEAR) {
+    } else if(mask & RTTimeMaskMonths && deltaSeconds < YEAR) {
         int months = (int)lroundf((float)deltaSeconds/(float)MONTH);
         return [self formattedStringForCurrentDate:currentDate count:months past:@"%d months ago" future:@"%d months from now"];
     } else if(deltaSeconds < 1.5*YEAR) {
